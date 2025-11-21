4256d883ad2f7424638c12fd40f0748b9533b8e1
Gihun Ham <progh2@gmail.com>
2025-11-21 16:39:01 +0900

    Uncomment user input and greeting functionality

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import streamlit as st
 st.title('나의 첫 웹 서비스 만들기!!')
 st.write('함샘과 함께하는 바이브 코딩~ 시작! 헬로 월드?!')
-#name = st.text_input('이름을 입력해주세요 : ')
-#menu = st.selectbox('좋아하는 음식을 선택해주세요:', ['망고빙수','아몬드봉봉'])
-#if st.button('인사말 생성') : 
-#  st.write(name+'님! 당신이 좋아하는 음식은 '+menu+'이군요?! 저도 좋아요!!')
+name = st.text_input('이름을 입력해주세요 : ')
+menu = st.selectbox('좋아하는 음식을 선택해주세요:', ['망고빙수','아몬드봉봉'])
+if st.button('인사말 생성') : 
+  st.write(name+'님! 당신이 좋아하는 음식은 '+menu+'이군요?! 저도 좋아요!!')
